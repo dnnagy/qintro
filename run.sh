346f811c3f8137c3100638c70ce43367692d2264
dnnagy <nagy@chromeshift.com>
2020-02-26 23:00:36 +0100

    Simulate 2-qubit systems

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 docker run -it -p $1:8888 \
---mount type=bind,source="$(pwd)",target=/home/nagyd96/workdir nagyd96/qiskit-notebook:2019-12-23-1
+--mount type=bind,source="$(pwd)",target=/home/nagyd96/workdir nagyd96/qiskit-notebook:2020-02-26-2
