2600487c8be24762ba233f7ba8362bd3080c66ee
dnnagy <nagy@chromeshift.com>
2020-03-08 16:20:55 +0100

    Test quantum-notebook docker image

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 docker run -it -p $1:8888 \
---mount type=bind,source="$(pwd)",target=/home/nagyd96/workdir nagyd96/qiskit-notebook:2020-03-08-1
+--mount type=bind,source="$(pwd)",target=/home/nagyd96/workdir nagyd96/quantum-notebook:2020-03-08-1
