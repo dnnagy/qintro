19ac7015d2744c6ce86ea2ffd2fbdf04a65207ad
dnnagy <nagy@chromeshift.com>
2020-03-08 14:34:06 +0100

    Update docker image

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 docker run -it -p $1:8888 \
---mount type=bind,source="$(pwd)",target=/home/nagyd96/workdir nagyd96/qiskit-notebook:2020-02-26-2
+--mount type=bind,source="$(pwd)",target=/home/nagyd96/workdir nagyd96/qiskit-notebook:2020-03-08-1
